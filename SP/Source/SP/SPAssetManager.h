62886d531108388f06fa63c8ed05e9b686652b7c
Junggun <lyngvi1@gmail.com>
2024-03-24 18:49:22 +0900

    assetmanager

---
+++ SP/Source/SP/SPAssetManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "SPAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SP_API USPAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+};
